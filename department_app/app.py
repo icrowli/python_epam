14a258c18e33e20a3cf7deda558ffaf246ab9293
icrowli <i.andriy.dovgalyuk@gmail.com>
2021-12-04 20:17:31 +0200

    Added 'app.py' file, added templates

---
+++ department_app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/texts/')
+def texts():
+    return render_template('texts.html')
+
+
+@app.route('/authors/')
+def authors():
+    return render_template('authors.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/check_text/')
+def check_text():
+    return render_template('check_text.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
